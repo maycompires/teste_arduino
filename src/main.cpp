cdf70e0f33508eb7cb3d91587edd5e0cbcc0b823
maycompires <maycompires@hotmail.com>
2023-09-07 22:10:53 -0300

    one

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,26 @@
 #include <arduino.h>
 
 int led1Pin = 9;
+int led2Pin = 10;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1Pin, OUTPUT);
+  pinMode(led2Pin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(5000);
-  digitalWrite(led1Pin, HIGH);
-  delay(5000);
-  digitalWrite(led1Pin, LOW);
   delay(1000);
   digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, LOW);
+  delay(2000);
+  digitalWrite(led1Pin, LOW);
+  digitalWrite(led2Pin, HIGH);
+  delay(2000);
+  digitalWrite(led1Pin, HIGH);
+  digitalWrite(led2Pin, HIGH);
   delay(300);
   digitalWrite(led1Pin, HIGH);    
+  digitalWrite(led2Pin, HIGH);
 }
